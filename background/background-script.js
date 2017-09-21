8d8ae3975615d5096eb1aa3f9f2a911df0579a6d
Black-Photon <jkeane2002@gmail.com>
2017-09-21 20:47:12 +0100

    Added inital background script file

---
+++ background/background-script.js
@@ -0,0 +1,6 @@
+console.log("x");
+console.log("Hello " + document.getElementsByClassName("action_bar_comments threaded_comments hidden").length);
+document.getElementsByClassName("view_comments comment_link");
+console.log("Hello " + document.getElementsByClassName("author_name").length);
+
+//D: D: D: D:
